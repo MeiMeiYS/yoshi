2e22f37fdf41974cb4845b47100ad21776da3ad9
meimei <shihmeiyin@gmail.com>
2022-01-06 10:40:35 -0800

    Set uproute error handler middleware, and delete node module package 'faker' due to faker having unpredictable issue right now

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+const { ValidationError } = require('sequelize');
 
 const { enviroment } = require('./config');
 const isProduction = enviroment === 'production';
@@ -39,6 +40,37 @@ app.use(
 //vvv Connected Routes
 app.use(routes);
 
+//vvv Error-Handlers
+// Catch unhandled requests and forward to error handler.
+app.use((req, res, next) => {
+    const err = new Error('The requested resource couldn\'t be found.');
+    err.title = 'Resource Not Found';
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    next(err);
+});
+
+// Process sequelize errors
+app.use((err, req, res, next) => {
+    // check if error is a Sequelize error:
+    if (err instanceof ValidationError){
+        err.errors = err.errors.map(e => e.message);
+        err.title = 'Validation error';
+    };
+    next(err);
+});
+
+// Error formatter
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    console.log(err);
+    res.json({
+        title: err.title || 'Server Error',
+        message: err.message,
+        errors: err.errors,
+        stack: isProduction ? null : err.stack,
+    });
+});
 
 
 
