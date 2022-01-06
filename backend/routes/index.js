21c121417598576c6ef5c4175d67992f720bb637
meimei <shihmeiyin@gmail.com>
2022-01-06 10:15:41 -0800

    Set up api route/router and created api test route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const apiRouter = require('./api');
 
+router.use('/api', apiRouter);
+
+// vvv test route
 router.get('/', (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
     res.send('hello! this is test route "/"');
