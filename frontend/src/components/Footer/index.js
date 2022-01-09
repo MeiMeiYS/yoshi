76c8f88def8aba6a96ec8e9d059a02da258046b9
meimei <shihmeiyin@gmail.com>
2022-01-08 23:38:54 -0800

    Modified footer and added css styling

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -1,9 +1,47 @@
-import './Footer.css'
+
+import './Footer.css';
 
 const Footer = () => {
+
+    const goToLinkedIn = e => {
+        e.preventDefault();
+        window.location.href = 'https://www.linkedin.com/in/meiyinshih/';
+
+    }
+
+    const goToGithub = e => {
+        e.preventDefault();
+        window.location.href = 'https://github.com/MeiMeiYS';
+    }
+
+    const goToMyWeb = e => {
+        e.preventDefault();
+        window.location.href = 'https://meiys.me/';
+    }
+
     return (
         <footer>
-            <h2>I am footer</h2>
+            <div className='footer-explore'>
+                <h4>Explore</h4>
+                <div className='footer-links'>
+                    <button
+                        type='button'
+                        onClick={goToLinkedIn}>
+                        <i className="fab fa-linkedin-in"></i>
+                    </button>
+                    <button
+                        type='button'
+                        onClick={goToGithub}>
+                        <i className="fab fa-github"></i>
+                    </button>
+                    <button
+                        type='button'
+                        onClick={goToMyWeb}>
+                        <i className="fab fa-medium-m"></i>
+                    </button>
+                </div>
+            </div>
+            <p>© 2021 | Designed & coded by Mei Shih</p>
         </footer>
     )
 }
