ac57744d305c7e11757dad3b635dd8351eda6c43
meimei <shihmeiyin@gmail.com>
2022-01-09 11:06:20 -0800

    Added Genre migrations and model and modified both files with constrains. Added Genre methods in models file. Modified User model by adding unique constrain.

---
+++ backend/db/models/genre.js
@@ -0,0 +1,29 @@
+'use strict';
+const { Validator } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  const Genre = sequelize.define('Genre', {
+    name: {
+      type: DataTypes.STRING(30),
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: [3, 30]
+      }
+    }
+  }, {});
+
+  Genre.associate = function(models) {
+    // associations can be defined here
+  };
+
+  Genre.getAllGenre = async function () {
+    return await Genre.findAll();
+  };
+
+  Genre.getGenreById = async function (id) {
+    return await Genre.findByPk(id);
+  };
+
+  return Genre;
+};
