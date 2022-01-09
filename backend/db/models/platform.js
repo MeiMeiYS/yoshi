210c16f01e633ee948a2d87c05bcc7292d4672c6
meimei <shihmeiyin@gmail.com>
2022-01-09 11:14:41 -0800

    Added platform migrations and model and modified both files with constrains. Added platform methods in models file.

---
+++ backend/db/models/platform.js
@@ -0,0 +1,29 @@
+'use strict';
+const { Validator } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  const Platform = sequelize.define('Platform', {
+    name: {
+      type: DataTypes.STRING(30),
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: [3, 30]
+      }
+    }
+  }, {});
+
+  Platform.associate = function(models) {
+    // associations can be defined here
+  };
+
+  Platform.getAllPlatform = async function () {
+    return await Platform.findAll();
+  };
+
+  Platform.getPlatformById = async function (id) {
+    return await Platform.findByPk(id);
+  };
+
+  return Platform;
+};
