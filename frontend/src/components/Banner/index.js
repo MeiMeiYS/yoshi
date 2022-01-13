ca9f1aa56a9659ac51d14bc9fb352627ae8cbbb0
meimei <shihmeiyin@gmail.com>
2022-01-13 15:26:31 -0800

    Fixed typo

--- frontend/src/components/Banner/index.js
+++ frontend/src/components/Banner/index.js
@@ -8,7 +8,7 @@ const Banner = () => {
         <div className='banner'>
             <img crossOrigin="anonymous" src='https://images.unsplash.com/photo-1534488972407-5a4aa1e47d83?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1123&q=80'></img>
             <div>
-                <h1 className="slogan" style={{color: 'white'}}>Find you gaming parters on <NavLink className='logo' exact to='/signup'>Yoshi</NavLink></h1>
+                <h1 className="slogan" style={{color: 'white'}}>Find your gaming partners on <NavLink className='logo' exact to='/signup'>Yoshi</NavLink></h1>
             </div>
         </div>
     )
